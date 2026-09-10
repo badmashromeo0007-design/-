400f1f0bc17b6f707559f1f51e11439c59e1bf1a
Badmash Romeo <badmashromeo0007@gmail.com>
2026-09-11 03:30:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -143,7 +143,7 @@ def handle_callback(call):
         )
         bot.edit_message_text(welcome_text, call.message.chat.id, call.message.message_id, reply_markup=markup, parse_mode="Markdown")
 
-@app.route(f'/{API_TOKEN}', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
@@ -157,7 +157,7 @@ def index():
 if __name__ == "__main__":
     bot.remove_webhook()
     time.sleep(1)
-    bot.set_webhook(url=f'{RENDER_URL}/{API_TOKEN}')
+    bot.set_webhook(url=f'{RENDER_URL}/webhook')
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
     
